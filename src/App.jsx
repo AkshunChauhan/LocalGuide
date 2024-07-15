a0bd2aee6e4e36f373e0082917fc915605e865f1
Akshun Chauhan <akshunchauhan099@gmail.com>
2024-07-15 00:34:34 -0400

    Updated design

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Blog from './pages/blog'
+import './design/global.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
