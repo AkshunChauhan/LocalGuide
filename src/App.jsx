a976b30ea60398e388700d0ee0ab3c89ec511c21
Shashwat Thakur <shashwat98210@gmail.com>
2024-07-16 12:36:06 -0600

    Design and develop 404 page not found with custom layout, CSS,and home page redirect button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Blog from './pages/blog'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Blog from "./pages/blog";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound from "./pages/notfound";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <Blog />
-      </div>
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Blog />} exact />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/pages/notfound.jsx
+++ src/pages/notfound.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./NotFound.css";
+import { NavLink } from "react-router-dom";
 
-export default function notfound() {
-    return (
-        <div>notfound</div>
-    )
+export default function NotFound() {
+	return (
+		<>
+			<div id="notfound">
+				<div className="notfound">
+					<div className="notfound-404">
+						<h1>404</h1>
+					</div>
+					<h2>we are sorry, page not found!</h2>
+					<p>
+						The page you are looking for might have been removed, had its name
+						change or is temporarily unavalable.
+					</p>
+					<NavLink to="/">Back TO Homepage </NavLink>
+				</div>
+			</div>
+		</>
+	);
 }
